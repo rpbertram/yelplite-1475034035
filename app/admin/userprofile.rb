256b58860f3580f200c01e294f1aa153fcebc33a
Raghu Betina <raghu@firstdraft.com>
2016-09-28 03:46:08 +0000

    Whitelist columns in Userprofile for ActiveAdmin

--- app/admin/userprofile.rb
+++ app/admin/userprofile.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Userprofile do
 
+ permit_params :firstname, :lastinitial, :avg_rating_given, :review_id, :user_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
